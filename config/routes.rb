ae7de2a67559de47881cc5012cc4c084736683e4
Kaarel Tinn <kaareltinn@hotmail.com>
2014-08-07 14:07:40 +0300

    Added registering feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Makeitsnappy::Application.routes.draw do
+
   root to: 'questions#index'
+  resources :users, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
