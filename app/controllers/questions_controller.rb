32dc8627b327ffe01272be6fd88a98ca757da2cf
Kaarel Tinn <kaareltinn@hotmail.com>
2014-08-08 00:24:17 +0300

    Added search feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -39,4 +39,8 @@ class QuestionsController < ApplicationController
       render 'edit'
     end
   end
+
+  def search
+    @questions = Question.search(params)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
-  
+
   attr_accessible :body, :solved
 
   validates :body, presence: true, length: { in: 10..255 }
@@ -9,5 +9,9 @@ class Question < ActiveRecord::Base
 
   def self.unsolved(params)
   	where(solved: false).paginate(page: params[:page], order: 'created_at DESC', per_page: 3)
+  end
+
+  def self.search(params)
+  	where("body LIKE ?", "%#{params[:keyword]}%").paginate(page: params[:page], order: 'created_at DESC', per_page: 3)
   end
 end
